78a83df16454aeb4cd2e970094abfdda87bd3c47
KavindiViranga <97431820+KavindiViranga@users.noreply.github.com>
2025-12-17 21:13:00 +0530

    add animation to home page

--- client/src/components/Description.jsx
+++ client/src/components/Description.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
 import { assets } from '../assets/assets'
+// eslint-disable-next-line no-unused-vars
+import { motion } from "motion/react"
 
 const Description = () => {
   return (
-    <div className='flex flex-col items-center justify-center my-23 p-6 md:px-28'>
+    <motion.div 
+    
+    initial={{opacity:0.2, y:100}}
+    transition={{duration:1}}
+    whileInView={{opacity:1, y:0}}
+    viewport={{once:true}}
+    
+    className='flex flex-col items-center justify-center my-23 p-6 md:px-28'>
         <h1 className='text-3xl sm:text-4xl font-semibold mb-2'>Create AI Images</h1>
         <p className='text-gray-500 mb-8'>Turn Your Imagination Into Visuals</p>
 
@@ -17,7 +26,7 @@ const Description = () => {
                 and watch your imagination unfold instantly.</p>
             </div>
         </div>
-    </div>
+    </motion.div>
   )
 }
 
--- client/src/components/Steps.jsx
+++ client/src/components/Steps.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
 import { stepsData } from '../assets/assets'
+// eslint-disable-next-line no-unused-vars
+import { motion } from "motion/react"
 
 const Steps = () => {
   return (
-    <div className='flex flex-col items-center justify-center my-32'>
+    <motion.div 
+    
+    initial={{opacity:0.2, y:100}}
+    transition={{duration:1}}
+    whileInView={{opacity:1, y:0}}
+    viewport={{once:true}}
+    
+    className='flex flex-col items-center justify-center my-32'>
         <h1 className='text-3xl sm:text-4xl font-semibols mb-2'>How it works</h1>
         <p className='text-lg text-gray-600 mb-8'>Transform Words Into Stunning Images</p>
 
@@ -20,7 +29,7 @@ const Steps = () => {
                 </div>
             ))}
         </div>
-    </div>
+    </motion.div>
   )
 }
 
